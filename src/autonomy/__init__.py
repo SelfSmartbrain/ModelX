b17b6508b647953d01ed63a513c21c23e64c4058
genius-0963 <geniusthegreat0963@gmail.com>
2026-06-25 00:58:53 +0530

    Update autonomy module initialization

--- src/autonomy/__init__.py
+++ src/autonomy/__init__.py
@@ -1,1 +1,11 @@
-# autonomy package
+"""Autonomy package exports."""
+
+from src.autonomy.objective_manager import Objective, ObjectiveManager
+from src.autonomy.progress_tracker import ProgressRecord, ProgressTracker
+
+__all__ = [
+    "Objective",
+    "ObjectiveManager",
+    "ProgressRecord",
+    "ProgressTracker",
+]
